579dedfa1bef71cb0b931c2f1d4504ba6ce2ecba
Vinaykumar <vinaykumartapela09@example.com>
2025-10-09 23:20:57 +0530

    Pascal Triangle Solution

---
+++ pascaltriangle.py
@@ -0,0 +1,11 @@
+def pascaltriangle(rows):
+    triangle = []
+    for i in range(rows):
+        row = [1] * (i + 1)
+        for j in range(1, i):
+            row[j] = triangle[i-1][j-1] + triangle[i-1][j]
+        triangle.append(row)
+    return triangle
+
+rows = int(input("Enter number of rows: "))
+print(pascaltriangle(rows))
