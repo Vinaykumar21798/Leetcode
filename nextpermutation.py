d8f70ba8f910e079de4f7df3d42ff28c8319a2f6
TapelaVinaykumar <vinaykumartapela09@gmail.com>
2025-10-10 23:19:46 +0530

    Next Permutation Solution

---
+++ nextpermutation.py
@@ -0,0 +1,18 @@
+def nextpermutation(nums):
+    n=len(nums)
+    i=n-2
+    while i>=0 and nums[i]>=nums[i+1]:
+        i-=1
+    if i>=0:
+        j=n-1
+        while nums[j]<=nums[i]:
+            j-=1
+        nums[j],nums[i]=nums[i],nums[j]
+    left,right=i+1,n-1
+    while left<=right:
+        nums[left],nums[right]=nums[right],nums[left]
+        left+=1
+        right-=1
+    return nums
+nums=list(map(int,input().split()))
+print(nextpermutation(nums))
